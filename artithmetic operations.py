bf94013808435404258ad9b78ede57489215d238
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-03-20 18:32:58 +0600

    basic functions

---
+++ artithmetic operations.py
@@ -0,0 +1,13 @@
+print(10/3) #float
+print(10//3) #int
+print(10%3) #returns reminder of the division
+print(10**3) #exponent
+
+x = 10
+x = x+3
+x += 3 #augmented assignment operator
+print(x)
+
+x = 10+3*2**2 #2**2 will be executed first
+print(x)
+
---
+++ math functions.py
@@ -0,0 +1,11 @@
+import math
+
+print(math.ceil(2.9)) #it rounds up the number
+print(math.floor(2.9))
+
+x = 2.9
+print(round(x)) #round function
+
+x = 2.9
+print(abs(-2.9)) #absolute always returns a positive number
+
