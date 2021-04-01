c83ff0515545b0fbb35f7528f0b2649b553b96b7
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-04-01 20:44:05 +0600

    parameters and reusable functions

--- parameters.py
+++ parameters.py
@@ -5,3 +5,17 @@ def greet_user(first_name, last_name):
 print("Start")
 greet_user("John", "Smith")
 print("Finish")
+
+
+###
+
+def square(number):
+    return number*number
+
+print (square(3))
+
+###
+def square(number):
+    print(number*number) 
+
+print (square(3))
