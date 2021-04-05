7c6ed06db36294d2be02583f31e5b65765d2d447
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-04-05 21:01:43 +0600

    modules

---
+++ converters.py
@@ -0,0 +1,15 @@
+#imported on modules.py
+def lbs_to_kg(weight):
+    return weight*.45
+
+def kg_to_lbs(weight):
+    return weight/.45
+
+
+
+
+from utils import find_max
+numbers = [10,3,6,2]
+maximum = find_max(numbers)
+
+print(maximum(numbers))
