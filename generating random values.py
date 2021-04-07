4dc0e807a793b71b1b26fc03f87e9c702fe3f589
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-04-07 21:29:37 +0600

    random module

---
+++ generating random values.py
@@ -0,0 +1,20 @@
+import random
+#random is a built in module
+
+for i in range(3):
+    print(random.random())
+    print(random.randint(10,20))
+
+members = ['John', 'Mary','Bob','Mosh']
+leader = random.choice(members)
+print(leader)
+
+class Dice:
+    def roll(self):
+        first = random.randint(1,6)
+        second = random.randint(1,6)
+        return first, second
+    
+dice = Dice()
+print(dice.roll())
+        
