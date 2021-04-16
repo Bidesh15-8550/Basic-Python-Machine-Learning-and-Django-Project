297c60ed8e9fdcd4ceb240007b3f999950fcfa96
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-04-16 11:25:52 +0600

    customizing models

--- Django/PyShop/products/views.py
+++ Django/PyShop/products/views.py
@@ -1,14 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 #/products -> index
 #Uniform Resource Locator(Address) 
 def index(request):
+    products = Product.objects.all()
     return HttpResponse('Hello World')
 
 
 def new(request):
     return HttpResponse('New Products')
+
+
 
 
 
