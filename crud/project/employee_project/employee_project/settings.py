6cfeeffda927e7898ecdf0de610852a9ca46cbfc
Bidesh Biswas Biki <bideshbiswas@ymail.com>
2021-06-27 01:05:48 +0600

    update

--- crud/project/employee_project/employee_project/settings.py
+++ crud/project/employee_project/employee_project/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'EmployeeDB',
         'USER': 'postgres',
-        'PASSWORD': '77323111b',
+        'PASSWORD': '773231',
         'HOST': 'localhost'
     }
 }
