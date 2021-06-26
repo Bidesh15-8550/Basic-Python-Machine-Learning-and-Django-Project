279b603a66bc72cb33f5ad48426aef6bd66fa5bf
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-06-27 00:33:13 +0600

    update

--- crud/project/employee_project/employee_register/views.py
+++ crud/project/employee_project/employee_register/views.py
@@ -6,7 +6,6 @@ def employee_list(request):
     return render(request, "employee_register/employee_list.html")
 
 
-
 def employee_form(request):
     form =  EmployeeForm()
     return render(request, "employee_register/employee_form.html",{'form':form})
